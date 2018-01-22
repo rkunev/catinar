e48e6bf416b80dd406edfb4701dbc48345d221cd
Rosen <rkunev@gmail.com>
2018-01-22 18:01:22 +0200

    build: Add Nuxt

---
+++ nuxt.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    head: {
+        title: 'nuxt-boilerplate',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: 'Nuxt.js project' },
+        ],
+        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    },
+
+    loading: { color: '#3B8070' },
+};
