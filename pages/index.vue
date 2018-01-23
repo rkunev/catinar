4bfc8c7cfd9d4e1c0b5c6129fdddf99787e2eca3
Rosen <rkunev@gmail.com>
2018-01-23 18:55:11 +0200

    feat: Add component for the cat

--- pages/index.vue
+++ pages/index.vue
@@ -1,59 +1,35 @@
 <template>
     <section class="container">
-        <div>
-            <app-logo />
-            <h1 class="title">nuxt-boilerplate</h1>
-            <h2 class="subtitle">Nuxt.js project</h2>
-            <div class="links">
-                <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-                    Documentation
-                </a>
-                <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">
-                    GitHub
-                </a>
-            </div>
+        <div class="kitty" v-for="n in 10">
+            <colorful-cat :editable="n % 2 === 0" />
         </div>
     </section>
-</template>;
-
+</template>
 
 <script>
-    import AppLogo from '~/components/AppLogo.vue';
+    import ColorfulCat from '@/components/ColorfulCat';
 
     export default {
-        components: {
-            AppLogo,
-        },
+        components: { ColorfulCat },
     };
 </script>
 
-<style>
+<style lang="scss">
     .container {
-        min-height: 100vh;
+        width: 960px;
+        margin-left: auto;
+        margin-right: auto;
         display: flex;
-        justify-content: center;
-        align-items: center;
-        text-align: center;
-    }
+        flex-wrap: wrap;
 
-    .title {
-        font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif; /* 1 */
-        display: block;
-        font-weight: 300;
-        font-size: 100px;
-        color: #35495e;
-        letter-spacing: 1px;
-    }
 
-    .subtitle {
-        font-weight: 300;
-        font-size: 42px;
-        color: #526488;
-        word-spacing: 5px;
-        padding-bottom: 15px;
-    }
+        .kitty {
+            padding: 15px;
+            width: 33.3333%;
 
-    .links {
-        padding-top: 15px;
+            > svg {
+                width: 100%;
+            }
+        }
     }
 </style>
