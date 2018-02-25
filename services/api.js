3050e8fbb17ae9b2a4118b6c4cf01e2d432ba42a
Rosen Kunev <rkunev@gmail.com>
2018-02-25 19:37:29 +0200

    refactor: Hoist data unwrap function

--- services/api.js
+++ services/api.js
@@ -5,6 +5,8 @@ const api = axios.create({
 });
 const suffix = '.json';
 
-export const create = (path, data = {}) => api.post(path + suffix, data).then(r => r.data);
-export const get = (path, params = {}) => api.get(path + suffix, { params }).then(r => r.data);
-export const put = (path, data = {}) => api.put(path + suffix, data).then(r => r.data);
+const unwrapData = r => r.data;
+
+export const create = (path, data = {}) => api.post(path + suffix, data).then(unwrapData);
+export const get = (path, params = {}) => api.get(path + suffix, { params }).then(unwrapData);
+export const put = (path, data = {}) => api.put(path + suffix, data).then(unwrapData);
